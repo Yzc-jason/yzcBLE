843de3003b114aea74182beef2b73cc8c8d0af80
叶志成 <yzc0253@gmail.com>
2016-07-28 21:50:24 +0800

    Initial Commit

---
+++ CoreBluetoothDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreBluetoothDemo
+//
+//  Created by 叶志成 on 16/7/28.
+//  Copyright © 2016年 叶志成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreBluetoothDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreBluetoothDemo
+//
+//  Created by 叶志成 on 16/7/28.
+//  Copyright © 2016年 叶志成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
